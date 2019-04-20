b079d799ae5fb06224f4232dbd8d32396545cddb
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-04-20 22:54:23 +0900

    Fix

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.hello = functions.https.onRequest((req, res) => res.write("hello"));
